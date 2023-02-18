16e2c47bd5e12bfe7675ef1a4bafcd47fd899ecb
JahangirShaikh <jahangirs203@gmail.com>
2023-02-18 22:11:50 +0530

    implemented addEmp method to EmployeeDAOimpl layer

--- GitDemo/src/com/lti/dao/EmployeeDAOImpl.java
+++ GitDemo/src/com/lti/dao/EmployeeDAOImpl.java
@@ -1,5 +1,13 @@
 package com.lti.dao;
 
+import com.lti.entity.Employee;
+
 public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Override
+	public Employee addEmployee(Employee e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
